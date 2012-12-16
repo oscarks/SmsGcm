8aaf7c18347e6526308cd9a1f870cada9ef143f4
Oscar Konno Sampaio <oscarks@globo.com>
2012-12-16 01:17:07 -0300

    Add tests

--- SmsGcmServer/test/unit/org/grails/plugins/smsgcmserver/SmsGcmDeviceRegisterControllerTests.groovy
+++ SmsGcmServer/test/unit/org/grails/plugins/smsgcmserver/SmsGcmDeviceRegisterControllerTests.groovy
@@ -8,7 +8,7 @@ import org.junit.*
 /**
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
-@TestFor(SmsGcmDeviceRegisterController)
+@TestFor(SmsGcmDeviceInterfaceController)
 class SmsGcmDeviceRegisterControllerTests {
 
     void testSomething() {
